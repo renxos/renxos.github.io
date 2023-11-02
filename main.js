9555555a1cc4974ccacec3fa35088e75a1a79407
renxos <149529403+renxos@users.noreply.github.com>
2023-11-02 20:04:54 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+// main.js
+// Ejemplo de desplazamiento suave al hacer clic en enlaces internos
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
